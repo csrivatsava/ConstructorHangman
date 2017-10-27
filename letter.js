62af52be7f965e4396d62532d6ed22b7c24db2a2
Srivatsava Chimakurthi <csrivatsava@CSri.local>
2017-10-27 13:17:34 -0700

    added the final changes to the constructor Hangman game

---
+++ letter.js
@@ -0,0 +1,56 @@
+
+// for defiing colors to the user input or output.
+var colors = require('colors')
+var line = colors.cyan("===========================================================================");
+
+
+var Letter = function(userGuess){
+	this.guess = userGuess;
+	this.cWord=[];
+	this.compareWord = compareWord;
+	this.checkLetter = checkLetter;
+  	
+}
+
+module.exports = Letter;
+
+function checkLetter(mainFunc){
+	 
+	 for (var i=0; i< mainFunc.randomWord.length;i++){
+	 		this.cWord[i]=mainFunc.currentWord[i]
+		}
+		mainFunc.userGuess.push(this.guess);
+		this.compareWord(this.guess, mainFunc)
+
+}
+
+function compareWord(letter, mainFunc){
+		var validGuess=false;
+
+		for (var i=0;i<mainFunc.randomWord.length;i++){
+			if (letter.toLowerCase() === mainFunc.randomWord[i]){
+				this.cWord[i] = mainFunc.randomWord[i];
+				validGuess = true;
+				mainFunc.currentWord = this.cWord;
+			}
+		}
+		if (validGuess){
+			console.log(line);
+			console.log(colors.green("CORRECT !!!"))
+			console.log("Your Word is: " + colors.yellow(mainFunc.currentWord.join(' ')))
+			console.log("Your Guesses is: " + colors.yellow(mainFunc.userGuess))
+			console.log(line);
+
+		}
+		else {
+			--mainFunc.choicesRemain;
+			console.log(line);
+			console.log(colors.red("INCORRECT !!!"))
+			console.log("Your Word is: " + colors.yellow(mainFunc.currentWord.join(' ')))
+			console.log("Your Guesses: " + colors.yellow(mainFunc.userGuess))
+			console.log(colors.green("Choices Remain: ") + mainFunc.choicesRemain );
+			console.log(line);
+
+		}
+
+	} // function compareWord end.
---
+++ word.js
@@ -0,0 +1,57 @@
+// for creating user prompts define inquirer
+var inquirer = require('inquirer');
+var Letter = require('./Letter');
+// for defiing colors to the user input or output.
+var colors = require('colors')
+var line = colors.cyan("===========================================================================");
+
+//New word object.
+var Word = function(wrd){
+    // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+	this.play = play
+}
+
+function play(mainFunc){
+
+	if ((mainFunc.choicesRemain!=0 && mainFunc.complete === false)){ 
+		console.log('Your Word is : ' + mainFunc.currentWord.join(' '))
+
+		inquirer.prompt([
+		{
+			type:'input',
+			name:"guess",
+			message:'Enter your Guess: '
+		}
+		]).then(function(uChoice){
+			console.log("userguess: " + uChoice.guess)
+			userGuess = uChoice.guess
+			var letter = new Letter(userGuess);
+			letter.checkLetter(mainFunc);
+
+			if(mainFunc.currentWord.join('') === mainFunc.randomWord){
+		        mainFunc.complete = true;
+		        mainFunc.wins++;
+		        mainFunc.printStats();
+		        mainFunc.startGame();
+		        return;     
+		    }
+		    else{
+		    	mainFunc.complete= false;
+		    	if (mainFunc.choicesRemain===0){
+		    		console.log(colors.red("Oh NOOOO!!! You Got it WRONG !!!"))
+	    			console.log(colors.yellow("CORRECT word is: " + mainFunc.randomWord))
+					console.log(line);	
+		    		mainFunc.losses++;
+		    		mainFunc.printStats();
+		    		mainFunc.startGame();
+		    		return;
+		    	}
+		    	play(mainFunc);        
+		    }
+			
+		})
+	}
+
+}
+
+module.exports = Word;
